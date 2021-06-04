ff6674c165d280962aecbedd297b1680f85aad95
natnael <natnaelmeresa6@gmail.com>
2021-06-04 20:08:49 +0300

    ludo.py edited

--- ludo.py
+++ ludo.py
@@ -4,6 +4,9 @@ Created on Sat Apr 17 17:49:50 2021
 
 @author: natnael
 """
+# how to move image to particular postion ; to move the objects from ther orgin
+# 
+# 
 
 
 from tkinter import *
@@ -163,7 +166,8 @@ def update(ind, ins):
 	if ind == frameCnt:
 		ind=0
 	if cunt > 40:
-		rand_int  = str(random.randint(1,6))
+		# rand_int  = str(random.randint(1,6))
+		rand_int= '6'
 		img = 'd-' + rand_int +'.png'
 		png2 = ImageTk.PhotoImage(Image.open(img))
 		btns[ins].configure(image=png2)
@@ -179,7 +183,31 @@ def update(ind, ins):
 #moving
 
 def move_obect(plyr, obj):
-	print(rand_int)
+	rand = int(rand_int)
+	print(plyr,'obj', obj)
+
+	if plyr == 0:
+		if obj_position[plyr][obj] == 0:
+			if  rand == 6:
+				canvas.move(player_obj_window[plyr][obj], 10, 10)
+		else:
+			x = True
+			for i in range(rand):
+				if obj_position[0][obj] in (0,5,11,13,18,24,26,31,37,39,49,50,51):
+					x = False
+				if x:
+					canvas.move(player_obj_window[plyr][obj], 15, 0)
+					root.update()
+			else:
+				canvas.move(player_obj_window[plyr][obj], 0, 15)
+				root.update()
+			time.sleep(0.1)
+				
+
+	
+	
+	else:
+		pass
 	
 
 # x - 60 , y = 60
@@ -189,36 +217,44 @@ player_obj = [
 ['player3_obj_1','player3_obj_2','player3_obj_3','player3_obj_4'],
 ['player4_obj_1','player4_obj_2','player4_obj_3','player4_obj_4']
 ]
+
+player_obj_window = [
+['player1_obj_1_window','player1_obj_2_window','player1_obj_3_window','player1_obj_4_window'],
+['player2_obj_1_window','player2_obj_2_window','player2_obj_3_window','player2_obj_4_window'],
+['player3_obj_1_window','player3_obj_2_window','player3_obj_3_window','player3_obj_4_window'],
+['player4_obj_1_window','player4_obj_2_window','player4_obj_3_window','player4_obj_4_window']
+]
+
 logo_img = ImageTk.PhotoImage(Image.open('logo.png'))
 
 #objects of first player
 player_obj[0][0]= Button(canvas, image=logo_img, command= lambda : move_obect(0,0))
-player1_ob_1_window = canvas.create_window(67,152, anchor="nw", window=player_obj[0][0])
+player_obj_window[0][0] = canvas.create_window(67,152, anchor="nw", window=player_obj[0][0])
 
 player_obj[0][1] = Button(canvas, image=logo_img, command= lambda : move_obect(0,1))
-player1_obj_2_window = canvas.create_window(127,152, anchor="nw", window=player_obj[0][1])
+player_obj_window[0][1] = canvas.create_window(127,152, anchor="nw", window=player_obj[0][1])
 
 
 player_obj[0][2] = Button(canvas, image=logo_img, command= lambda : move_obect(0,2))
-player1_obj_3_window = canvas.create_window(67,212, anchor="nw", window=player_obj[0][2])
+player_obj_window[0][2] = canvas.create_window(67,212, anchor="nw", window=player_obj[0][2])
 
 player_obj[0][3] = Button(canvas, image=logo_img, command= lambda : move_obect(0,3))
-player1_obj_4_window = canvas.create_window(127,212, anchor="nw", window=player_obj[0][3])
+player_obj_window[0][3] = canvas.create_window(127,212, anchor="nw", window=player_obj[0][3])
 
 #objects of second player
 
 player_obj[1][0]= Button(canvas, image=logo_img, command= lambda : move_obect(1,0))
-player1_ob_1_window = canvas.create_window(338,152, anchor="nw", window=player_obj[1][0])
+player_obj_window[1][0] = canvas.create_window(338,152, anchor="nw", window=player_obj[1][0])
 
 player_obj[1][1] = Button(canvas, image=logo_img, command= lambda : move_obect(1,1))
-player1_obj_2_window = canvas.create_window(398,152, anchor="nw", window=player_obj[1][1])
+player_obj_window[1][1] = canvas.create_window(398,152, anchor="nw", window=player_obj[1][1])
 
 
 player_obj[1][2] = Button(canvas, image=logo_img, command= lambda : move_obect(1,2))
-player1_obj_3_window = canvas.create_window(338,212, anchor="nw", window=player_obj[1][2])
+player_obj_window[1][2] = canvas.create_window(338,212, anchor="nw", window=player_obj[1][2])
 
 player_obj[1][3] = Button(canvas, image=logo_img, command= lambda : move_obect(1,3))
-player1_obj_4_window = canvas.create_window(398,212, anchor="nw", window=player_obj[1][3])
+player_obj_window[1][3] = canvas.create_window(398,212, anchor="nw", window=player_obj[1][3])
 
 #objects of thrid player
 
